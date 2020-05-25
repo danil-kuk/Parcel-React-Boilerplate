5c08f979d03e110357b96c122a59f56fe0c91832
danil-kuk <danilkuk5@gmail.com>
2020-05-25 15:38:32 +0500

    Update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,16 @@ const About = () => {
   return (
     <div className='about'>
       <h1>About</h1>
+      <h3>Show async fetch() data in console</h3>
+      <button onClick={getData}>Get Data</button>
     </div>
   )
 }
 
 export default About
+
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  const json = await response.json()
+  console.log(json)
+}
